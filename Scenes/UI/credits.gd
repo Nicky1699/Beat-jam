02bca0629b9239974dac5a7345141fedfd7e2d81
Rudr <kzoldiack@gmail.com>
2025-04-08 17:01:35 +0530

    credits_added

--- Scenes/UI/credits.gd
+++ Scenes/UI/credits.gd
@@ -3,3 +3,7 @@ extends Node2D
 
 func _on_timer_timeout() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main/main_page.tscn")
+
+
+func _on_texture_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/main/main_page.tscn")
--- Scenes/main/main_page.gd
+++ Scenes/main/main_page.gd
@@ -22,4 +22,4 @@ func _on_exit_pressed() -> void:
 
 
 func _on_credits_pressed() -> void:
-	pass
+	get_tree().change_scene_to_file("res://Scenes/UI/credits.tscn")
